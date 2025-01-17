2c3c9b854152cca6bc5acd5905d15bb579cdc010
Andy Griffiths <griffiths_andy@icloud.com>
2025-01-17 17:40:11 +0000

    11.1 complete

--- src/point.rs
+++ src/point.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+// use std::ops::Add;
 
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
 pub struct Point<T> {
@@ -6,15 +6,15 @@ pub struct Point<T> {
     pub y: T,
 }
 
-impl<T> Point<T> {
-    pub fn nesw(pos: Point<T>) -> Vec<Point<T>>
-        where for<'a> &'a T: Add<i32>
-    {
-        let mut res: Vec<Point<T>> = Vec::new();
-        for (dx, dy)  in [(0, -1), (1, 0), (0, 1), (-1, 0)] {
-            let next_pos = Point { x: &pos.x + dx, y: &pos.y + dy };
-            res.push(next_pos);
-        }
-        res
-    }
-}
+// impl<T> Point<T> {
+//     pub fn nesw(pos: Point<T>) -> Vec<Point<T>>
+//         where for<'a> &'a T: Add<i32>
+//     {
+//         let mut res: Vec<Point<T>> = Vec::new();
+//         for (dx, dy)  in [(0, -1), (1, 0), (0, 1), (-1, 0)] {
+//             let next_pos: Point<i32> = Point { x: &pos.x + dx, y: &pos.y + dy };
+//             res.push(next_pos);
+//         }
+//         res
+//     }
+// }
