61c198885558a4871c0d19331b8f655457ff7c24
Andy Griffiths <griffiths_andy@icloud.com>
2025-02-09 22:15:04 +0000

    13.1 - complete

--- src/divisors.rs
+++ src/divisors.rs
@@ -1,12 +1,25 @@
+use num::abs;
+
 // Euclid's Algorithm for greatest common divisor
-pub fn gcd(a: usize, b: usize) -> usize {
+pub fn gcd(a: i32, b: i32) -> i32 {
     if b == 0 {
         a
     } else {
+        // Just for explanation purposes while looking at Diophantine equations
+        println!("{} - ({} * {}) = {}", a, abs(a / b), b, a % b);
         gcd(b, a % b)
     }
 }
 
-pub fn lcm(a: usize, b: usize) -> usize {
+pub fn lcm(a: i32, b: i32) -> i32 {
     (a / gcd(a, b)) * b
+}
+
+mod tests {
+    use crate::divisors::gcd;
+
+    #[test]
+    fn gcd_test() {
+        dbg!(gcd(94, 22));
+    }
 }
